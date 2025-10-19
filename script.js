750f2e06e79a7eea08965f35acc924fc9768820e
RylandMatis <momatis3@gmail.com>
2025-10-19 00:14:37 -0500

    First main work

---
+++ script.js
@@ -0,0 +1,6 @@
+ document.addEventListener('DOMContentLoaded', function() {
+        console.log('JavaScript is running!');
+        // You can add more interactive elements here
+        // alert('Hello from JavaScript!');
+    });
+
